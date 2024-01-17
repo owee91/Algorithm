9161526669060e1c6a52064041094a91c565862d
owee91 <102273446+owee91@users.noreply.github.com>
2024-01-17 23:37:26 +0900

    Time: 63 ms (44.01%) | Memory: 17.4 MB (18.21%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -2,17 +2,13 @@ class Solution:
     def isPalindrome(self, x: int) -> bool:
         result = False
         if x < 0 :
-            print(x)
             result = False
         else :
             revers = int(str(x)[::-1])
-            print(type(x))
-            print(type(revers))
             if x == revers :
                 result = True
             else:
                 result = False
-        print(result)
         return result
             
                 
